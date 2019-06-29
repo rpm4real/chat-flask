96060e498084f553714ead6cc1f21f63b3c8afbc
Molloy, Riley <rpm4real@gmail.com>
2019-06-29 13:40:24 -0400

    Creating initial app file and home html page

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+
+app = Flask(__name__)
+
+bot = ChatBot("Alfred")
+bot.set_trainer(ListTrainer)
+bot.set_trainer(ChatterBotCorpusTrainer)
+bot.train("chatterbot.corpus.english")
+
+
+@app.route("/")
+def home():    
+    return render_template("home.html") 
+@app.route("/get")
+def get_bot_response():    
+    userText = request.args.get('msg')    
+    return str(bot.get_response(userText)) 
+if __name__ == "__main__":    
+    app.run()
